943db73bf254b0e85f63f9f62cec76c2f6f9ef35
Muthash <kathingo92@gmail.com>
2018-03-04 10:39:09 +0300

    Setup user test class [Starts #155522102]

---
+++ app/tests/test_user.py
@@ -0,0 +1,18 @@
+import unittest
+import json
+from app import create_app
+
+
+class AuthTestCase(unittest.TestCase):
+    """Test case for the user creation and login"""
+
+    def setUp(self):
+        """Set up test variables."""
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
+        self.user_data = {
+            'email': 'test@example.com',
+            'username': 'stephen',
+            'password': 'test_password',
+            'cpassword': 'test_password'
+        }
