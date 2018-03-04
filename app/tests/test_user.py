918515b9a1a5c353b74de8fbc5f9d6f2889c3964
Muthash <kathingo92@gmail.com>
2018-03-05 01:05:23 +0300

    convsert form data to json format

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -20,13 +20,15 @@ class AuthTestCase(unittest.TestCase):
 
     def test_registration(self):
         """Test user registration works correcty."""
-        res = self.client().post('/api/v1/register', data=self.user_data)
+        res = self.client().post('/api/v1/register',
+                                 headers={'Content-Type': 'application/json'},
+                                 data=json.dumps(self.user_data))
         result = json.loads(res.data.decode())
         self.assertEqual(result['message'], "You registered successfully")
         self.assertEqual(res.status_code, 201)
         new_account = self.user_data['email'] in USERS.keys()
         self.assertTrue(new_account)
-    
+
     def test_already_registered_user(self):
         """Test that a user cannot be registered twice."""
         user_data = {
@@ -35,10 +37,14 @@ class AuthTestCase(unittest.TestCase):
             'password': 'test_password',
             'cpassword': 'test_password'
         }
-        res = self.client().post('/api/v1/register', data=user_data)
+        res = self.client().post('/api/v1/register',
+                                 headers={'Content-Type': 'application/json'},
+                                 data=json.dumps(user_data))
         self.assertEqual(res.status_code, 201)
-        second_res = self.client().post('/api/v1/register',
-                                        data=user_data)
+        second_res = self.client().post(
+            '/api/v1/register',
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps(user_data))
         result = json.loads(second_res.data.decode())
         self.assertEqual(result['message'], "User already exists." +
                          "Please login")
@@ -52,23 +58,33 @@ class AuthTestCase(unittest.TestCase):
             'password': 'test_password',
             'cpassword': 'test_password'
         }
-        res = self.client().post('/api/v1/register', data=self.user_data)
+        res = self.client().post(
+            '/api/v1/register',
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps(self.user_data))
         self.assertEqual(res.status_code, 201)
-        user_data = {
+        login_data = {
             'email': 'steve@example.com',
             'password': 'test_password'
         }
-        login_res = self.client().post('/api/v1/login', data=user_data)
+        login_res = self.client().post(
+            '/api/v1/login',
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps(login_data))
         result = json.loads(login_res.data.decode())
         self.assertEqual(result['message'], "You logged in successfully")
         self.assertEqual(login_res.status_code, 200)
+        self.assertTrue(result['access_token'])
 
     def test_unregistered_user_login(self):
         user_data = {
             'email': 'unreg@example.com',
             'password': 'password'
         }
-        login_res = self.client().post('/api/v1/login', data=user_data)
+        login_res = self.client().post(
+            '/api/v1/login',
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps(user_data))
         result = json.loads(login_res.data.decode())
         self.assertEqual(result['message'], "This user does not exist." +
                          " Please register")
@@ -82,14 +98,19 @@ class AuthTestCase(unittest.TestCase):
             'password': 'password',
             'cpassword': 'password'
         }
-        res = self.client().post('/api/v1/register', data=self.user_data)
+        res = self.client().post(
+            '/api/v1/register',
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps(self.user_data))
         self.assertEqual(res.status_code, 201)
         user_data = {
             'email': 'wrong@example.com',
             'password': 'test_password'
         }
-        login_res = self.client().post('/api/v1/login', data=user_data)
+        login_res = self.client().post(
+            '/api/v1/login',
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps(user_data))
         result = json.loads(login_res.data.decode())
         self.assertEqual(result['message'], "Invalid email or password")
         self.assertEqual(login_res.status_code, 401)
-
