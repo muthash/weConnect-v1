0b148430fae9c74a2ce31d3641a27bcf0af6867e
Muthash <kathingo92@gmail.com>
2018-03-04 10:36:25 +0300

    import views after registering blueprint

--- app/v1/__init__.py
+++ app/v1/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-from v1 import views
 
 # This instance of a Blueprint that represents the version blueprint
 v1 = Blueprint('v1', 'v1', url_prefix='/api/v1')
+
+from . import views
