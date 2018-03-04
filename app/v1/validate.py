3d3f989a705fc7d25e058581559053d779e6d72a
Muthash <kathingo92@gmail.com>
2018-03-05 01:02:24 +0300

    update check_mail method to validate inline spacing

--- app/v1/validate.py
+++ app/v1/validate.py
@@ -4,7 +4,8 @@ import re
 def check_email(email):
     """ Method to check if email entered is valid """
     blank_input = email != ''
-    newmail = email.strip()
+    stripmail = email.strip()
+    newmail = re.sub(r'\s+', '', stripmail)
     inline_space = len(newmail) == len(email)
     regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$)"
     pattern = bool(re.search(regex, email))
