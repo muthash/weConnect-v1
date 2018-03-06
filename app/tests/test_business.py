067ada03663f4d9ce98fb4aa6ff0bc8bbc20a5b1
Muthash <kathingo92@gmail.com>
2018-03-06 16:49:02 +0300

    test business can be added successfully

--- app/tests/test_business.py
+++ app/tests/test_business.py
@@ -1,7 +1,6 @@
 import unittest
 import json
 from app import create_app
-from app.v1.user import USERS
 
 
 class AuthTestCase(unittest.TestCase):
@@ -11,8 +10,14 @@ class AuthTestCase(unittest.TestCase):
         """Set up test variables."""
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
-    
-    def register_user(self, email="user@test.com", username="stephen", password="test1234"):
+        self.business = {
+            'businessId': '1',
+            'businessName': 'Kenya Power',
+            'category': 'Lighting',
+            'location': 'Nairobi'
+        }
+
+    def register_user(self, email="business@test.com", username="stephen", password="test1234"):
         """This helper method helps register a test user."""
         user_data = {'email': email, 'username': username, 'password': password}
         return self.client().post(
@@ -20,7 +25,8 @@ class AuthTestCase(unittest.TestCase):
                 headers={'Content-Type': 'application/json'},
                 data=json.dumps(user_data)
                )
-    def login_user(self, email="user@test.com", password="test1234"):
+
+    def login_user(self, email="business@test.com", password="test1234"):
         """This helper method helps log in a test user."""
         user_data = {'email': email, 'password': password}
         return self.client().post(
@@ -28,3 +34,36 @@ class AuthTestCase(unittest.TestCase):
                 headers={'Content-Type': 'application/json'},
                 data=json.dumps(user_data)
                )
+
+    def test_business_creation(self):
+        """Test the API can create a bussiness (POST request)"""
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+        res = self.client().post(
+            '/api/v1/businesses',
+            headers={'Content-Type': 'application/json',
+                     'Authorization': 'Bearer ' + access_token},
+            data=json.dumps(self.business))
+        bizIds = json.loads(res.data.decode())['business']
+        self.assertIn("1", bizIds)
+        self.assertEqual(res.status_code, 201)
+
+    def test_get_all_businesses(self):
+        """Test the API can create a bussiness (POST request)"""
+        self.register_user()
+        result = self.login_user()
+        access_token = json.loads(result.data.decode())['access_token']
+        res = self.client().post(
+            '/api/v1/businesses',
+            headers={'Content-Type': 'application/json',
+                     'Authorization': 'Bearer ' + access_token},
+            data=json.dumps(self.business)
+        )
+        res = self.client().get(
+            '/api/v1/businesses',
+            headers={'Authorization': 'Bearer ' + access_token}
+        )
+        biz = json.loads(res.data.decode())
+        self.assertTrue(biz)
+        self.assertEqual(res.status_code, 201)
