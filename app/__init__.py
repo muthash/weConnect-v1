786834757f298b2864f6457e02ce34c4d05a3676
muthash <kathingo92@gmail.com>
2018-04-14 20:37:10 +0300

    Add error handlers route

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,12 @@
     returns it after it's loaded up with configuration settings
     using app.config
 """
-
+from flask import jsonify
 from flask_api import FlaskAPI
-
-# local import
+from flask_jwt_extended import JWTManager
 from instance.config import app_config
+
+jwt = JWTManager()
 
 
 def create_app(config_name):
@@ -16,9 +17,41 @@ def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    jwt.init_app(app)
 
-    # import the version blueprint and register it on the app
-    from .v1 import v1
-    app.register_blueprint(v1)
+    blacklist = set()
+
+    from app.auth.views import auth
+    # from app.business.views import biz, rev
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        """Error handler for a bad request"""
+        return jsonify(dict(error='The Server did not understand the request')), 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        """Error handler for not found page"""
+        return jsonify(dict(error='Resource not found')), 404
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        """Error handler for wrong method to an endpoint"""
+        return jsonify(dict(error='The HTTP request Method is not allowed')), 405
+
+    @app.errorhandler(500)
+    def server_error(error):
+        """Error handler for a server failure"""
+        return jsonify(dict(error='Database connection failed! Try Again')), 500
+
+    @jwt.token_in_blacklist_loader
+    def check_if_token_in_blacklist(decrypted_token):
+        """Check if token is blaclisted before allowing access to a route"""
+        jti = decrypted_token['jti']
+        return jti in blacklist
+
+    app.register_blueprint(auth)
+    # app.register_blueprint(biz)
+    # app.register_blueprint(rev)
 
     return app
