af6efeaf3b01bef7494b7afbc9d96d48e2a3113a
muthash <stephen.kathingo@andela.com>
2018-04-16 00:31:20 +0300

    Add post review endpoint

--- app/business/views.py
+++ app/business/views.py
@@ -7,6 +7,7 @@ from app.baseview import BaseView
 from app.auth.views import users
 
 biz = Blueprint('biz', __name__, url_prefix='/api/v1/businesses')
+rev = Blueprint('rev', __name__, url_prefix='/api/v1/businesses/<int:business_id>/reviews')
 store = []
 
 
@@ -17,11 +18,10 @@ class BusinessManipulation(BaseView):
         if not self.validate_json():
             data = request.get_json()
             name = data.get('name')
-            description = data.get('description')
             category = data.get('category')
             location = data.get('location')
             current_user = get_jwt_identity()
-            data_ = dict(name=name, description=description, category=category, location=location)
+            data_ = dict(name=name, category=category, location=location)
             if not self.validate_null(**data_):
                 user_ = [user for user in users if current_user == user.email]
                 if user_:
@@ -41,24 +41,22 @@ class BusinessManipulation(BaseView):
         if not self.validate_json():
             data = request.get_json()
             name = data.get('name')
-            description = data.get('description')
             category = data.get('category')
             location = data.get('location')
             current_user = get_jwt_identity()
-            data_ = dict(name=name, description=description, category=category, location=location)
+            data_ = dict(name=name, category=category, location=location)
             if not self.validate_null(**data_):
-                business_ = [business for business in store if business_id == business.id]
-                if business_:
-                    business = business_[0]
-                    if current_user == business.created_by:
-                        data = self.remove_extra_spaces(**data_)
-                        index = store.index(business)
-                        del store[index]
-                        store.insert(index, business)
-                        response = {'message': 'Business updated successfully'}
-                        return jsonify(response), 200
-                    response = {'message': 'The operation is forbidden for this business'}
-                    return jsonify(response), 403
+                for business in store:
+                    if business_id == business.id:
+                        if current_user == business.created_by:
+                            data = self.remove_extra_spaces(**data_)
+                            business.name = data['name']
+                            business.category = data['category']
+                            business.location = data['location']
+                            response = {'message': 'Business updated successfully'}
+                            return jsonify(response), 200
+                        response = {'message': 'The operation is forbidden for this business'}
+                        return jsonify(response), 403
                 response = {'message': 'The business {} is not available'.format(business_id)}
                 return jsonify(response), 404
             return self.validate_null(**data_)
@@ -115,7 +113,42 @@ class BusinessManipulation(BaseView):
             return jsonify(response), 404
 
 
+class ReviewManipulation(BaseView):
+    """Method to manipulate business endpoints"""
+    @jwt_required
+    def post(self, business_id):
+        """Endpoint to save the data to the database"""
+        if not self.validate_json():
+            data = request.get_json()
+            review = data.get('review')
+            current_user = get_jwt_identity()
+            data_ = dict(review=review)
+            if not self.validate_null(**data_):
+                user_ = [user for user in users if current_user == user.email]
+                if user_:
+                    data = self.remove_extra_spaces(**data_)
+                    for business in store:
+                        if business_id == business.id:
+                            if current_user == business.created_by:
+                                response = {'message': 'The operation is forbidden for own business'}
+                                return jsonify(response), 403
+                            business.reviews.append(data['review'])
+                            response = {'message': 'Review for business {} created'.format(business_id)}
+                            return jsonify(response), 201
+                        response = {'message': 'The business {} is not available'.format(business_id)}
+                    return jsonify(response), 404
+                response = {'message': 'Please login to continue'}
+                return jsonify(response), 401
+            return self.validate_null(**data_)
+        return self.validate_json()
+                        
+
+
 business_view = BusinessManipulation.as_view('businesses')
 biz.add_url_rule('', defaults={'business_id':None}, view_func=business_view, methods=['GET',])
 biz.add_url_rule('', view_func=business_view, methods=['POST',])
-biz.add_url_rule('/<int:business_id>', view_func=business_view, methods=['GET', 'PUT', 'DELETE'])
+biz.add_url_rule('/<int:business_id>', view_func=business_view, methods=['GET', 'PUT', 'DELETE',])
+biz.add_url_rule('/<int:business_id>/reviews', view_func=business_view, methods=['GET'])
+
+review_view = ReviewManipulation.as_view('reviews')
+rev.add_url_rule('', view_func=review_view, methods=['POST'])
